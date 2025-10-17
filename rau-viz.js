7a60be7dc491b11193510271fef86d6d410a0273
cellophane <87787413+cello-phane@users.noreply.github.com>
2025-10-17 12:00:54 -0700

    Update rau-viz.js

--- rau-viz.js
+++ rau-viz.js
@@ -595,8 +595,8 @@ document.addEventListener('DOMContentLoaded', () => {
   }
   
   // Call for each canvas
-  setupResponsiveCanvas('canvas1', 0.642);
-  setupResponsiveCanvas('canvas2', 0.542);
+  setupResponsiveCanvas('canvas1', 1.0);
+  setupResponsiveCanvas('canvas2', 0.642);
 
 
 
