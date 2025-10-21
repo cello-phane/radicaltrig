241781b2c119fdfddda451db5bbcf0bffa760433
Michael L <87787413+cello-phane@users.noreply.github.com>
2025-10-21 02:55:49 -0700

    Update rau-viz.js

--- rau-viz.js
+++ rau-viz.js
@@ -546,7 +546,7 @@ document.addEventListener('DOMContentLoaded', () => {
       ctx.arc(centerX, centerY, arcRadius, -endAngle, -startAngle);
       ctx.stroke();
       const refvec = {x: 1.0, y: 0.0};
-      if (atanVec(refvec, u) < atanVec(u, v) + Math.abs(atanVec(refvec, u) - atanVec(refvec, v)) && (uVal/90) < atanVec(refvec, v)) {
+      if (rauPhase > endAngle && atanVec(refvec, u) < atanVec(u, v) + Math.abs(atanVec(refvec, u) - atanVec(refvec, v)) && (uVal/90) < atanVec(refvec, v)) {
         console.log("Blue(v) is switching direction.");
         currentPhaseSection2 = 4.0 - rauPhase;
       }
