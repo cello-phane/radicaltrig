09fc17334fe7a3fe8ac4daae9d861b1c78caa95a
Michael L <87787413+cello-phane@users.noreply.github.com>
2025-10-20 21:39:31 -0700

    Update rau-viz.js

--- rau-viz.js
+++ rau-viz.js
@@ -507,11 +507,11 @@ document.addEventListener('DOMContentLoaded', () => {
       const vVal = (parseInt(controls.vAngle.value) / 90.0);
       
       const usc = getRotationComponents(uVal);
-      const vsc = getRotationComponents(-vVal);
+      const vsc = getRotationComponents(vVal);
       //const u = {x: uLen*Math.cos(uAng), y: -uLen*Math.sin(uAng)};
       //const v = {x: vLen*Math.cos(vAng), y: -vLen*Math.sin(vAng)};
       const u = {x: uLen*usc.cos_result, y: -uLen*usc.sin_result};
-      const v = {x: vLen*vsc.cos_result, y: -vLen*vsc.sin_result};
+      const v = {x: vLen*vsc.cos_result, y: vLen*vsc.sin_result};
       const rauPhase = atanVec(u, v);
       
       currentU = u;
