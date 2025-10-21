c36ea4350d7cd5284becf499cafb1521c461875f
Michael L <87787413+cello-phane@users.noreply.github.com>
2025-10-20 23:45:21 -0700

    Update rau-viz.js

--- rau-viz.js
+++ rau-viz.js
@@ -543,10 +543,10 @@ document.addEventListener('DOMContentLoaded', () => {
       ctx.lineWidth = 2;
       ctx.beginPath();
       if (Math.sign(u.x * v.y - u.y * v.x) > 0) {
-        ctx.arc(centerX, centerY, arcRadius, Math.min(uAng, vAng) , Math.max(uAng, vAng));
+        ctx.arc(centerX, centerY, arcRadius, uAng , Math.max(uAng, vAng));
       }
       else {
-        ctx.arc(centerX, centerY, arcRadius, -Math.max(uAng, vAng) , -Math.min(uAng, vAng));
+        ctx.arc(centerX, centerY, arcRadius, uAng , Math.min(uAng, vAng));
       }
       ctx.stroke();
 
