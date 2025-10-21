9c466c3a0842a99e2f1f4d522e85e09c511e577d
Michael L <87787413+cello-phane@users.noreply.github.com>
2025-10-21 00:14:48 -0700

    Update rau-viz.js

--- rau-viz.js
+++ rau-viz.js
@@ -537,8 +537,8 @@ document.addEventListener('DOMContentLoaded', () => {
       drawArrow(ctx, centerX, centerY, vEnd.x, vEnd.y, '#4444ff', 3);
 
       const arcRadius = Math.min(uLen, vLen);
-      const uAngle = Math.sign(u.x * v.y - u.y * v.x)*uAng;
-      const vAngle = vAng;
+      const uAngle = uAng;
+      const vAngle = Math.sign(u.x * v.y - u.y * v.x)*vAng;
       ctx.strokeStyle = '#666';
       ctx.lineWidth = 2;
       ctx.beginPath();
