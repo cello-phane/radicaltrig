03e3a0310fed28e224fc6ffa069bcf3efd35efb4
Michael L <87787413+cello-phane@users.noreply.github.com>
2025-10-22 05:10:12 -0700

    Update rau-viz.js

--- rau-viz.js
+++ rau-viz.js
@@ -583,7 +583,7 @@ document.addEventListener('DOMContentLoaded', () => {
       rauPhase = atanVec(u,v);
       currentPhaseSection2 = rauPhase;
       anglebetweenDeg = Math.abs(parseInt(controls.uAngle.value) - parseInt(controls.vAngle.value));
-
+      if (anticlockwise) anglebetweenDeg = 360 - anglebetweenDeg;
       updateResultsDisplay();
       updateConversionDisplay();
     }
