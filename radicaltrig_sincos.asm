7e4636d9e1fdd49ec1ceecce810ec9cfea3a6c34
Michael L <87787413+cello-phane@users.noreply.github.com>
2026-09-18 12:57:16 -0700

    Fix comments in radicaltrig_sincos.asm
    Correct comments for Estrin scheme polynomial evaluation.

--- radicaltrig_sincos.asm
+++ radicaltrig_sincos.asm
@@ -53,7 +53,7 @@ sincos_rau:
 
     ; -------- Estrin --------
     ; Optional: use Estrin scheme(instead of linear Horner to evaluate the polynomial):
-	; p0123 = {(c0*v^2 + c1) + (v^4 * (c2 + v^2 + c3)} + *v^ + c5}
+	; p0123 = {(c0 + v^2 * c1)} + {(v^4 * (c2 + v^2 + c3)}
 	; p = {p0123} + {(v^8 * (c4 + v^2 * c5)}
     ; p0123 = {([.coef0] + xmm4 * [.coef1])}=xmm5 + {xmm4*xmm4 * ([.coef2] + xmm4 * [.coef3])} = xmm0
     ; p     = p0123 + {xmm4*xmm4*xmm4*xmm4 * ([c.oef4] + xmm4 * [.coef5])} = xmm1
